ed04bbd28a409456b00955a5abb0325d28f3a18f
Hugo <Hugo>
2017-05-04 08:10:40 -0300

    documentação do pipeline

--- pipeline.lua
+++ pipeline.lua
@@ -1,7 +1,22 @@
+--[[
+Title: Programming Style Exercise - Pipeline
+Authors: Hugo Rodrigues Manhães (Writer), Jordan Rodrigues Rangel (Reviewer)
+Date: 29/04/2017
+Version: 1.0
+Size: 8.192 bytes
+]]--
+
 function readFile(pathToFile)
 	--[[
-	Takes a path to a file and returns the entire
-	contents of the file as a string
+	Pre-condition: The function receives a valid non-empty string that
+				   corresponds to a valid existent text file
+	Post-condition: The function correctly reads the file and has put all
+				   its' contents to a string. 
+	Argumentation: Considering that the pre-condition is true, the call to the 
+				   native function io.open is correct, because it receives
+				   a valid path to a file, therefore it will correctly 
+				   open the file. The read function is also working correctly
+				   because the file variable is correctly a opened file.
 	]]--
 	
 	local file = io.open(pathToFile, "rb")
@@ -13,8 +28,17 @@ end
 
 function filterCharsAndNormalize(stringData)
 	--[[
-	Takes a string and return a copy with all nonalphanumeric
-	chars replaced by white space
+	Pre-condition: The function receives the entire contents
+				   of a file on a string
+    Post-condition: The function has taken out all non-alphanumeric
+					characters and substituted them with a white space,
+					and also substituted all uppercase letters with 
+					their lowercase counterparts
+	Argumentation: Considering that the pre-condition is true, the string
+				   is non-empty and as such, the native gsub function will
+				   work, since the Regular Expression is equivalent to all
+				   non-alphanumeric characters, and the substitution is the
+				   white space. The same can be said of the native lower function.
 	]]--
 	
 	normalizedString = string.gsub(stringData, '[^a-zA-Z0-9]', ' ')
@@ -25,8 +49,15 @@ end
 
 function scan(stringData)
 	--[[
-	Takes a string and scans for words, returning
-	a list of words.
+	Pre-condition: The string has no non-alphanumeric characters separated by
+				   white spaces and is all lowercase.
+	Post-condition: The function generates a table in which every entry
+					is a word of the original string.
+	Argumentation: Considering that the pre-condition is true, the string 
+				   will have all words separated by white spaces, which is
+				   the pre-requisite for the native function gmatch to get
+				   all words, which in turn will have the "for" insert all
+				   words into the table that is the return value of the function.
 	]]--
 	
 	wordTable = {}
@@ -40,8 +71,19 @@ end
 
 function removeStopWords(wordTable)
 	--[[
-	Takes a list of words and returns a copy with all stop
-	words removed
+	Pre-condition: Every element of the table passed as an argument
+				   is a lowercase word.
+	Post-condition: The returned table has all the original elements
+				    of the original table save for all stop words contained
+					in the stop_words.txt file.
+	Argumentation: The same argumentation of the readFile function can be made for 
+				   the opening of the stop_words file. Considering this to be true, the 
+				   stringData will contain all stop words. By the same argumentation of 
+				   the scan function, the stopWordsTable variable will have all stop words as
+				   elements of the table. Considering this to also be true, then the nested
+				   "for" loop will, for every word of the argument table, search for its'
+				   existence in the stop words table. If it is, it won't be put in the
+				   returned table. This will lead to the post-condition.
 	]]--
 	
 	local file = io.open("stop_words.txt", "rb")
@@ -81,8 +123,14 @@ end
 
 function frequencies(noStopWordsTable)
 	--[[
-	Takes a list of words and returns a dictionary associating
-	words with frequencies of occurence
+	Pre-condition: The argument table will have only non-stop
+				   words as its' elements.
+	Post-condition: The returned table will have pairs of word/frequency,
+					in which each frequency will be correctly the correponding
+					number of occurrences of said word in the argument table.
+	Argumentation: The "for" parses through the entirety of the table elements only once,
+				   and as such it will only count each occurrence once, and so by the end
+				   of it, it will have correctly counted all occurrences of each word.
 	]]--
 	
 	wordFrequencies = {}
@@ -100,27 +148,34 @@ end
 
 function printAllSorted(wordFrequencies)
 	--[[
-	Takes a dictionary of words and their frequencies
-	and prints a list of pairs where the entries are 
-	sorted by frequency
+	Pre-condition: The argument table has only word/frequency pairs for elements.
+	Post-condition: The function prints the 25 most frequent words and their 
+					correspondent frequency.
+	Argumentation: Considering that the compare function used in the native sort
+				   function is correct, in the sense that it correctly points out
+				   which of two words has more occurrences and thefore bigger frequency,
+				   the sort function will correctly order the keyTable table. As such, 
+				   since the "for" will pass through all elements of the keyTable table
+				   in the correct order, it will correctly print the 25 most frequent words
+				   and their frequencies.
 	]]--
 	
 	function compare(a,b)
 		return wordFrequencies[a] > wordFrequencies[b]
 	end
 	
-	local tkeys = {}
+	local keyTable = {}
 	
-	for k in pairs(wordFrequencies) do table.insert(tkeys, k) end
+	for key in pairs(wordFrequencies) do table.insert(keyTable, key) end
 	
-	table.sort(tkeys,compare)
+	table.sort(keyTable,compare)
 	
 	twentyFifthFlag = 0
-	for _, k in pairs(tkeys) do
+	for _, key in pairs(keyTable) do
 		if twentyFifthFlag == 25 then
 			break
 		end
-		print(k .. " - " .. wordFrequencies[k])
+		print(key .. " - " .. wordFrequencies[key])
 		twentyFifthFlag = twentyFifthFlag + 1
 	end
 end
